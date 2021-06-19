e1383b4e41e16e312a32631945b426fe39263dc1
OurdiaD <daideche.o@gmail.com>
2021-06-19 16:45:39 +0200

    temp user

--- Mareu/app/src/main/java/com/lamzone/mareu/ui/list_meeting/AddMeetingActivity.java
+++ Mareu/app/src/main/java/com/lamzone/mareu/ui/list_meeting/AddMeetingActivity.java
@@ -1,5 +1,6 @@
 package com.lamzone.mareu.ui.list_meeting;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.Spanned;
@@ -13,10 +14,12 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipDrawable;
 import com.google.android.material.datepicker.CalendarConstraints;
 import com.google.android.material.datepicker.DateValidatorPointForward;
@@ -40,9 +43,11 @@ import java.util.Locale;
 import java.util.Objects;
 import java.util.TimeZone;
 
+import static android.view.KeyEvent.ACTION_MULTIPLE;
 import static android.view.KeyEvent.KEYCODE_COMMA;
 import static android.view.KeyEvent.KEYCODE_SEMICOLON;
 import static android.view.KeyEvent.KEYCODE_SPACE;
+import static android.view.inputmethod.EditorInfo.IME_ACTION_PREVIOUS;
 import static android.view.inputmethod.EditorInfo.IME_NULL;
 
 public class AddMeetingActivity extends AppCompatActivity {
@@ -113,12 +118,19 @@ public class AddMeetingActivity extends AppCompatActivity {
     }
 
     public void initHourView(){
-        final MaterialTimePicker timePicker = new MaterialTimePicker.Builder()
+        /*final MaterialTimePicker timePicker = new MaterialTimePicker.Builder()
                 .setTitleText("Select hour")
                 .setTimeFormat(TimeFormat.CLOCK_24H)
-                .build();
+                .build();*/
 
-        timePicker.show(getSupportFragmentManager(), "time");
+        TimePickerDialog timePicker = new TimePickerDialog(this,listenerTime,10, 10, true);
+        timePicker.setTitle("select hour");
+        timePicker.create();
+        timePicker.show();
+
+
+
+        /*timePicker.show(getSupportFragmentManager(), "time");
         timePicker.addOnPositiveButtonClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -130,7 +142,7 @@ public class AddMeetingActivity extends AppCompatActivity {
                 String dateSelected = date + " " + hour;
                 binding.date.setText(dateSelected);
             }
-        });
+        });*/
     }
 
     public void initParticipantsView(){
@@ -143,30 +155,30 @@ public class AddMeetingActivity extends AppCompatActivity {
         participantsTextView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
         participantsTextView.setThreshold(1);
         participantsTextView.performValidation();
-        //participantsTextView.onEditorAction(IME_NULL);
-        //participantsTextView.setOnEditorActionListener(userEditionListener);
-        participantsTextView.addTextChangedListener(watcherMail);
-        /*participantsTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        //participantsTextView.addTextChangedListener(watcherMail);
+        participantsTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 User selectedUser = (User) adapterView.getItemAtPosition(i);
-                createRecipientChip(selectedUser.getMail());
+                createRecipientChip(selectedUser);
             }
-        });*/
+        });
     }
 
-    private void createRecipientChip(String selectedUser) {
+    private void createRecipientChip(User selectedUser) {
         ChipDrawable chip = ChipDrawable.createFromResource(this, R.xml.standalone_chip);
         ImageSpan span = new ImageSpan(chip);
         int cursorPosition = participantsTextView.getSelectionStart();
-        Log.d("lol selected user",selectedUser.length()+" "+ cursorPosition);
-        int spanLength = selectedUser.length() + 1;
+        int spanLength = selectedUser.getMail().length() + 2;
         Editable text = participantsTextView.getText();
-        chip.setText(selectedUser);
-
+        chip.setText(selectedUser.getMail());
         chip.setBounds(0, 0, chip.getIntrinsicWidth(), chip.getIntrinsicHeight());
         text.setSpan(span, cursorPosition - spanLength, cursorPosition, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-        //text.setSpan(span, 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        /*Chip chip = new Chip(this);
+        chip.setText(selectedUser);
+        binding.participantsMeeting.addView(chip);*/
+
     }
 
     private final View.OnClickListener listenerCreate = new View.OnClickListener() {
@@ -191,123 +203,50 @@ public class AddMeetingActivity extends AppCompatActivity {
     };
 
 
-    private final TextWatcher watcherMail = new TextWatcher() {
+    /*private final TextWatcher watcherMail = new TextWatcher() {
         boolean change;
         CharSequence newtext;
         int sizeLast=0;
         int sizeCurrent=0;
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            Log.d("lol", "before " + start + " "+ after + " "+ count + " " +s);
-
-            if (after == 0){
-                sizeCurrent = count;
-                change = true;
-            }
 
         }
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            String check = String.valueOf(s.subSequence(s.length() -1, s.length()));
-            //String check = String.valueOf(s.charAt(before));
-            Log.d("lol", "change " + start + " "+ before + " "+ count + " " +s+ "-" +check+"-" +check.equals(" ") );
-            if (check.equals(" ") || check.equals(",") || check.equals(";")){
-                change = true;
-                //if (count >sizeCurrent+1){
-                    sizeCurrent = count;
-               // }
-
-            } else {
-                sizeCurrent = count;
-            }
-            /*if (before == 0 && count > 1){
-                if (change){
-                    change = false;
-                } else {
+            if (s.length() > 0 ){
+                String check = String.valueOf(s.subSequence(s.length() -1, s.length()));
+                //String check = String.valueOf(s.subSequence(0, count));
+                if (check.equals(" ") || check.equals(",") || check.equals(";")){
                     change = true;
                 }
-            } else {
-                sizeCurrent = count;
-            }*/
+            }
         }
 
         @Override
         public void afterTextChanged(Editable s) {
-            Log.d("lol", "after " + s.toString() + " "+change);
             if (change){
-                String list = String.valueOf(s);
+                *//*String list = String.valueOf(s);
                 String[] arrList = list.split(" ");
                 String user = arrList[arrList.length - 1];
-                /*
-                int lastIndex = list.lastIndexOf(" ",);
-                String user = list.substring(lastIndex);*/
-
-                /*Log.d("lol chip", sizeLast +"-"+sizeCurrent);
-                Log.d("lol chip", ""+s.subSequence(sizeLast, sizeLast+sizeCurrent));
-                String user = String.valueOf(s.subSequence(sizeLast, sizeLast+sizeCurrent));
-                */
                 user = user.replace(",","");
-                createRecipientChip(user);
+                createRecipientChip(user);*//*
+                createRecipientChip(String.valueOf(s));
                 change = false;
-                sizeLast = s.length();
+                //sizeLast = s.length();
+                s.clear();
             }
-        }
-    };
-
-  /*  private TextWatcher textWather = new TextWatcher() {
-        int noOfCharAdded=0;int noOfCharDeleted=0;
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-            startIdx=start;
-        }
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count,int after) {
-            noOfCharAdded=after;
-            noOfCharDeleted=count;
-        }
-        @Override
-        public void afterTextChanged(Editable s) {
-            Editable buffer = s;
-            int start = multiContentText.getSelectionStart()<0?0:multiContentText.getSelectionStart();
-            int end = multiContentText.getSelectionEnd()<0?0:multiContentText.getSelectionEnd();
-            if(noOfCharAdded==0 && noOfCharDeleted==1){ //if space is deleted
-                if (start == end && delPrevText) {
-                    ImageSpan link[] = buffer.getSpans(start, end,ImageSpan.class);
-                    if (link.length > 0) {
-                        buffer.replace(buffer.getSpanStart(link[0]),buffer.getSpanEnd(link[0]),"");
-                        buffer.removeSpan(link[0]);
-                    }
-                }
-                delPrevText=true;
-                multiContentText.setSelection(multiContentText.getText().length());
-            }
-            else if(noOfCharAdded==0 && noOfCharDeleted>1){//if the whole word is deleted
-                if(buffer.length()>0){
-                    if(start<buffer.length()){
-                        delPrevText=false;
-                        if(buffer.charAt(start)==' '){
-                            buffer.replace(start,start+1,"");
-                        }
-                    }
-                }
-            }
-
         }
     };*/
 
-    private final MultiAutoCompleteTextView.OnEditorActionListener userEditionListener = new MultiAutoCompleteTextView.OnEditorActionListener() {
+    TimePickerDialog.OnTimeSetListener listenerTime = new TimePickerDialog.OnTimeSetListener() {
         @Override
-        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-            Log.d("lol editor", actionId + " " + KEYCODE_SPACE + " " + event.getAction() + " "+ event.getKeyCode());
-            if (actionId == KEYCODE_SPACE ||
-                    actionId == KEYCODE_COMMA ||
-                    actionId == KEYCODE_SEMICOLON){
-                createRecipientChip(v.toString());
-                return true;
-            }
-            return false;
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            hour = hourOfDay + ":" + minute;
+
+            String dateSelected = date + " " + hour;
+            binding.date.setText(dateSelected);
         }
     };
-
 }
