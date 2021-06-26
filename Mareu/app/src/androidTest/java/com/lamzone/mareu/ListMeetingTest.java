c4d2324510b133a6686cc85ea6bb821f4989f789
OurdiaD <daideche.o@gmail.com>
2021-06-26 13:44:29 +0200

    listmeeting test

--- Mareu/app/src/androidTest/java/com/lamzone/mareu/ListMeetingTest.java
+++ Mareu/app/src/androidTest/java/com/lamzone/mareu/ListMeetingTest.java
@@ -1,6 +1,7 @@
 package com.lamzone.mareu;
 
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -24,7 +25,12 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
 
 import androidx.test.espresso.contrib.RecyclerViewActions;
 
@@ -54,6 +60,9 @@ public class ListMeetingTest {
 
     @Test
     public void meetingsList_checkData(){
+       onView(allOf(withId(R.id.meeting), withText("Reunion A - 18/05/2021 - 14:00 - Peach"),
+                withParent(withParent(withId(R.id.meeting_list))),
+                isDisplayed()));
 
     }
 
@@ -64,6 +73,12 @@ public class ListMeetingTest {
 
         onView(ViewMatchers.withId(R.id.meeting_list))
                 .check(matches(hasChildCount(itemCount -1)));
+    }
+
+    @Test
+    public void clickAdd_openActivity(){
+        onView(withId(R.id.add_meeting)).perform(click());
+        onView(withId(R.id.save_meeting));
     }
 }
 
